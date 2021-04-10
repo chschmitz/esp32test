d504dab9cbf84d175a1ce6eaed30fae5cadda35e
Christoph Schmitz <cs@cs75.de>
2021-04-10 22:13:37 +0200

    Working except actually executing macros.

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,10 +1,13 @@
 #define ONBOARD_LED 2
 
 void setup() {
+    Serial.begin(115200);
     pinMode(ONBOARD_LED, OUTPUT);
+    Serial.println("setup");
 }
 
 void loop() {
+    Serial.println("Loop");
     delay(500);
     digitalWrite(ONBOARD_LED, HIGH);
     delay(500);
--- duet-controller/duet-controller.ino
+++ duet-controller/duet-controller.ino
@@ -1,10 +1,3 @@
-/**
- * BasicHTTPClient.ino
- *
- *  Created on: 24.05.2015
- *
- */
-
 #include <Arduino.h>
 
 #include <WiFi.h>
@@ -14,9 +7,25 @@
 
 #include "credentials.h"
 
+#include <splash.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT.h>
+#include <gfxfont.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SPITFT_Macros.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 String CMD_AUTH = "/rr_connect?password=";
 String CMD_ON = "/rr_gcode?gcode=M106%20P2%20S255%20M106%20P3%20S255"; 
 String CMD_OFF = "/rr_gcode?gcode=M106%20P2%20S0%20M106%20P3%20S0";
+String CMD_STATUS = "/rr_status?type=1"; 
 
 #define USE_SERIAL Serial
 
@@ -28,16 +37,13 @@ const int SIGNAL_LED = 2;
 String URL_AUTH = String(DUET_URL);
 String URL_ON = String(DUET_URL);
 String URL_OFF = String(DUET_URL);
+String URL_STATUS = String(DUET_URL);
 
 void setup() {
   pinMode(TOGGLE_BUTTON, INPUT_PULLUP);
   pinMode(SIGNAL_LED, OUTPUT);
   attachInterrupt(TOGGLE_BUTTON, isr, FALLING);
   USE_SERIAL.begin(115200);
-
-  USE_SERIAL.println();
-  USE_SERIAL.println();
-  USE_SERIAL.println();
 
   for(uint8_t t = 4; t > 0; t--) {
     USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
@@ -50,10 +56,66 @@ void setup() {
   URL_AUTH.concat(DUET_PASSWD);
   URL_ON.concat(CMD_ON);
   URL_OFF.concat(CMD_OFF);
+  URL_STATUS.concat(CMD_STATUS);
 
   USE_SERIAL.printf("Auth URL: %s\n", URL_AUTH.c_str());
   USE_SERIAL.printf("On URL: %s\n", URL_ON.c_str());
   USE_SERIAL.printf("Off URL: %s\n", URL_OFF.c_str());  
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  } 
+  display.clearDisplay();
+}
+ 
+String sub(String what, String fromMark, String toMark) {
+    int fromPos = what.indexOf(fromMark);
+    if (fromPos < 0) {
+      return "";
+    }
+    int toPos = what.indexOf(toMark, fromPos + fromMark.length());
+    if (toPos < 0) {
+      return "";
+    }
+    return what.substring(fromPos + fromMark.length(), toPos);
+}
+
+int onOff = 1;
+
+void printStatus() {
+  display.clearDisplay();
+  display.setTextSize(1); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("Licht: ");
+  display.println(!onOff); 
+
+  if((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    http.begin(URL_AUTH);
+    http.GET();
+    http.end();
+
+    http.begin(URL_STATUS);  
+    int httpCode = http.GET();
+
+    if(httpCode > 0) {
+      if(httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        USE_SERIAL.println(payload);
+        display.print("Status: ");
+        display.println(sub(payload, "{\"status\":\"", "\""));
+        display.print("Temp: "); 
+        display.println(sub(payload, ",\"current\":[", "]"));
+      }  
+    } else {
+      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();
+  }
+
+  display.display();
 }
 
 void makeRequest(HTTPClient& http, String url) {
@@ -65,7 +127,6 @@ void makeRequest(HTTPClient& http, String url) {
     USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
     if(httpCode == HTTP_CODE_OK) {
       String payload = http.getString();
-      USE_SERIAL.println(payload);
     }
   } else {
     USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
@@ -73,8 +134,12 @@ void makeRequest(HTTPClient& http, String url) {
   http.end();
 }
 
-int onOff = 1;
-void makeRequest() {
+int buttonPressed = 0;
+void isr() {
+  buttonPressed = 1;
+}
+
+void toggleLight() {
   if((wifiMulti.run() == WL_CONNECTED)) {
 
     HTTPClient http;
@@ -92,25 +157,14 @@ void makeRequest() {
     }
     http.end();
   }
-
   onOff = !onOff;
 }
 
-int lastInterrupt = 0;
-int wait = 1;
-void isr() {
-  if (millis() - lastInterrupt > 50) {
-    wait = 0;
-  }
-  lastInterrupt = millis();  
-}
-
 void loop() {
-  while (wait) {
-    // FIXME: there must be a better way to synchronize this
-    delay(100);
+  if (buttonPressed) {
+    toggleLight();
+    buttonPressed = 0;    
   }
-  USE_SERIAL.print("Button pressed");
-  makeRequest();
-  wait = 1;
+  delay(5000);
+  printStatus();
 }
